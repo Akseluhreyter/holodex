41639159413826c5a8f1a673a112cd1145d63bd7
Akseluhreyter <abryan39@vols.utk.edu>
2023-08-08 05:49:51 -0400

    Change required resource parameters from keyword args to positional

--- lib/holodex/resource/related_videos.rb
+++ lib/holodex/resource/related_videos.rb
@@ -1,4 +1,4 @@
 class Holodex
   # https://docs.holodex.net/#operation/get-v2-channels-channelId-clips
-  def related_videos(id:, type:, **) = get("channels/#{id}/#{type}", **)
+  def related_videos(id, type, **) = get("channels/#{id}/#{type}", **)
 end
